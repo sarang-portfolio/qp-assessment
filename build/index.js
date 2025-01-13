9be5774af19b45d85aeeba4769ca1b6c0ddca76e
sarangkulkarni99 <sarang.kulkarni99@gmail.com>
2025-01-13 22:19:40 +0530

    added: dotenv package

--- build/index.js
+++ build/index.js
@@ -5,6 +5,8 @@ var __importDefault =
     return mod && mod.__esModule ? mod : { default: mod };
   };
 Object.defineProperty(exports, '__esModule', { value: true });
+const dotenv_1 = require('dotenv');
+(0, dotenv_1.config)();
 const app_1 = __importDefault(require('./app/app'));
 (0, app_1.default)();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { config } from 'dotenv';
+config();
 import startServer from './app/app';
 
 startServer();
